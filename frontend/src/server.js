7a21a22a388bf91135b181815ea1858d607f971e
Tayyab <tayyabali50055@gmail.com>
2025-09-29 11:49:58 +0500

    server.js in frontend update

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000/api/v2";
+export const server ="https://multi-vendor-ecommerce-backend-seven.vercel.app/api/v2";
 
-export const backend_url = "http://localhost:8000";
+export const backend_url ="https://multi-vendor-ecommerce-backend-seven.vercel.app";
